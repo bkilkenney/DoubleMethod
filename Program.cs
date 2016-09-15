206370050442a717e8e7260d13664505c8903680
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-15 11:46:52 -0400

    Not displaying multiplied result

--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ namespace DoubleMethod
     {
         static void Main(string[] args)
         {
+            /* Create a method named "Double" to calculate and return an integer number doubled. 
+            for example. Double(7) should return 14 */
+
+            
+            Console.WriteLine("Select a number");
+            
+           
+        }
+        static int Double(int number)
+            //Method double is asking for an INT NUMBER.  NUMBER is a temporary variable
+
+        {
+            int answer = number * 2;
+            Console.WriteLine(answer);
+                
+            return answer;
         }
     }
 }
